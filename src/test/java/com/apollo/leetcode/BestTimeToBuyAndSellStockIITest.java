00949f98187889cc992fa147afba03fc18c9b858
Apollo917 <omgapollon917@gmail.com>
2021-11-18 21:15:55 +0200

    122. Best Time to Buy and Sell Stock II solved

---
+++ src/test/java/com/apollo/leetcode/BestTimeToBuyAndSellStockIITest.java
@@ -0,0 +1,31 @@
+package com.apollo.leetcode;
+
+import com.apollo.leetcode.medium.BestTimeToBuyAndSellStockII;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BestTimeToBuyAndSellStockIITest implements ArgumentsProvider {
+
+    @ParameterizedTest
+    @ArgumentsSource(BestTimeToBuyAndSellStockIITest.class)
+    public void test(int[] input, int expectedResult) {
+        BestTimeToBuyAndSellStockII obj = new BestTimeToBuyAndSellStockII();
+        assertEquals(expectedResult, obj.start(input));
+    }
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+        return Stream.of(
+                Arguments.of(new int[]{7, 1, 5, 3, 6, 4}, 7),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 4),
+                Arguments.of(new int[]{7, 6, 4, 3, 1}, 0)
+        );
+    }
+}
