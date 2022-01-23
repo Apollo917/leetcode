31ca25e1a76ef007d8372b0728ec3b5307629878
Apollo917 <omgapollon917@gmail.com>
2022-01-23 20:54:37 +0200

    Solution One

--- src/main/java/com/apollo/leetcode/medium/SequentialDigits.java
+++ src/main/java/com/apollo/leetcode/medium/SequentialDigits.java
@@ -1,6 +1,6 @@
 package com.apollo.leetcode.medium;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,6 +14,23 @@ import java.util.List;
 public class SequentialDigits {
 
     public List<Integer> sequentialDigits(int low, int high) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        final String sequence = "123456789";
+
+        int lowWindowWidth = String.valueOf(low).length();
+        int highWindowWidth = String.valueOf(high).length();
+
+        for (int currentWidth = lowWindowWidth; currentWidth <= highWindowWidth; currentWidth++) {
+
+            for (int offset = 0; offset + currentWidth <= sequence.length(); offset++) {
+                int value = Integer.parseInt(sequence.substring(offset, offset + currentWidth));
+
+                if (value >= low && value <= high) {
+                    result.add(value);
+                }
+            }
+        }
+
+        return result;
     }
 }
