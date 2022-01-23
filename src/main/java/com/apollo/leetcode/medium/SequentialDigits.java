a32b5420f4d31f755be98ae64d83bfd68dc0e8bd
Apollo917 <omgapollon917@gmail.com>
2022-01-23 20:40:40 +0200

    Init

---
+++ src/main/java/com/apollo/leetcode/medium/SequentialDigits.java
@@ -0,0 +1,19 @@
+package com.apollo.leetcode.medium;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1291. Sequential Digits
+ * <p>
+ * An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+ * Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+ *
+ * @see <a href="https://leetcode.com/problems/sequential-digits/">Sequential Digits<a/>
+ */
+public class SequentialDigits {
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        return Collections.emptyList();
+    }
+}
