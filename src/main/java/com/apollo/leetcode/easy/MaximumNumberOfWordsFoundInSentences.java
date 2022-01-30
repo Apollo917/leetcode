4e3886372f3c4d5c7235c5f565a602cd57eee523
Apollo917 <omgapollon917@gmail.com>
2022-01-30 13:15:27 +0200

    Init

---
+++ src/main/java/com/apollo/leetcode/easy/MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,17 @@
+package com.apollo.leetcode.easy;
+
+/**
+ * 2114. Maximum Number of Words Found in Sentences
+ * <p>
+ * A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+ * You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+ * Return the maximum number of words that appear in a single sentence.
+ *
+ * @see <a href="https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/">Maximum Number of Words Found in Sentences<a/>
+ */
+public class MaximumNumberOfWordsFoundInSentences {
+
+    public int mostWordsFound(String[] sentences) {
+        return 0;
+    }
+}
